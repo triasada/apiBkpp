8275d3960ed730d62102a2ccb45fb21799115d7c
mukticahya <mukticahya@192.168.100.14>
2017-07-12 01:23:06 +0700

    test

---
+++ BkppRestFulServices-Api/src/java/com/bkpp/ws/api/LoginController.java
@@ -0,0 +1,30 @@
+package com.bkpp.ws.api;
+
+
+import com.bkpp.ws.model.LoginModel;
+import com.bkpp.ws.model.SingleConn;
+import com.bkpp.ws.model.vo.login.LoginRequest;
+import com.bkpp.ws.model.vo.login.LoginResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Mukti Cahya.
+ * @since July 12, 2017
+ */
+@RestController
+public class LoginController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ResponseEntity<LoginResponse> doLogin(@RequestBody LoginRequest request) {
+        SingleConn conn = new SingleConn();
+        LoginModel model = new LoginModel(conn.ds);
+        LoginResponse response = model.login(request);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
---
+++ BkppRestFulServices-Api/web/WEB-INF/jsp/home.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : home
+    Created on : Jan 30, 2017, 4:42:25 PM
+    Author     : root
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
