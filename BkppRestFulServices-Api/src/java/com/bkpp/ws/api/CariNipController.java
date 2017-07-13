44e91a425c64f6eebe365669a41756815fb2995e
novryanizm <novryanizm@localhost>
2017-07-13 09:36:00 +0700

    model query

---
+++ BkppRestFulServices-Api/src/java/com/bkpp/ws/api/CariNipController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bkpp.ws.api;
+
+import com.bkpp.ws.model.BkppModel;
+import com.bkpp.ws.model.SingleConn;
+import com.bkpp.ws.model.vo.cariNip.CariNipRequest;
+import com.bkpp.ws.model.vo.cariNip.CariNipRespons;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author novryanizm
+ */
+@RestController
+public class CariNipController {
+
+    @RequestMapping(value = "/cariNip", method = RequestMethod.POST)
+    public ResponseEntity<CariNipRespons> cariNip(@RequestBody CariNipRequest request) {
+        SingleConn conn = new SingleConn();
+        BkppModel model = new BkppModel(conn.ds);
+        CariNipRespons respons = model.getCariNip(request);
+        return new ResponseEntity<>(respons, HttpStatus.OK);
+    }
+}
